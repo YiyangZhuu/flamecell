4bf23cd88a8a0228968fa3ba6ae15485153af9f4
YiyangZhuu <yiyang.zhu@rwth-aachen.de>
2025-06-05 22:12:20 +0200

    raster_to_grid added

--- src/flamecell/sim_utils.py
+++ src/flamecell/sim_utils.py
@@ -1,3 +1,5 @@
+from flamecell import Grid
+
 def raster_to_cell(pixel_value):
     if pixel_value < 50:
         return "WATER"
@@ -7,3 +9,14 @@ def raster_to_cell(pixel_value):
         return "TREE"
     else: 
         return "GRASS"
+
+
+def raster_to_grid(data):
+    height, width = data.shape
+    grid = Grid(width, height)
+    for y in range(height):
+        for x in range(width):
+            state = raster_to_cell(data[y,x])
+            grid.cells[y][x].state = state
+            grid.cells[y][x].health 3 if state == "TREE" else 0
+    return grid
