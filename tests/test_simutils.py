450b73c24e57fd281d2c18719d9c44fe4cd94a91
YiyangZhuu <yiyang.zhu@rwth-aachen.de>
2025-06-20 14:13:09 +0200

    init test try 6

--- tests/test_simutils.py
+++ tests/test_simutils.py
@@ -1,5 +1,6 @@
-from flamecell import sim_utils
+import pytest
+from flamecell.sim_utils import Grid
 
 def test_grid_init():
-    grid = Grid(width=10, height=10)
+    grid = Grid(10, 10)
     assert grid.state.shape == (10, 10)
